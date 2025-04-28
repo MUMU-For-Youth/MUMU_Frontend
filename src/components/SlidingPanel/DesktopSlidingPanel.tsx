687bc505727a78a55f37ec199589664b746b25e9
Mooni <88727410+moon0727@users.noreply.github.com>
2025-04-28 20:36:02 +0900

    feat: SlidingPanel 컴포넌트 추가(데탑, 모바일 버전)
    SlidingPanel 컴포넌트 추가:
    - useScreenStore를 활용하여 화면 크기에 따라 패널의 위치와 크기를 동적으로 조정
    - DesktopSlidingPanel, MobileSlidingPanel 분리하여 index.tsx 생성
    - 데스크탑 화면에서는 왼쪽에 width: 300px의 패널을, 모바일 화면에서는 아래쪽에 width: 100%의 패널을 렌더링
    - 데스크탑은 클릭을 기준으로 2단계, 모바일은 슬라이드 기준으로 3단계의 상태 존재
    
    페이지 업데이트:
    - SlidingPanel 컴포넌트 EducationMap, SpaceMap, Mypage 페이지에 추가하여 지도와 함께 사용할 수 있도록 레이아웃 구성.
    - Mypage 작업 위해 로그인 true로 설정
    
    기타:
    - @use-gesture/react 설치 => 슬라이드 관련
    - react-spring 설치 => 슬라이드 관련
    - react 버전 다운
    - App.tsx 스크롤 비활성화
    - ArrowIcon 두개 추가

---
+++ src/components/SlidingPanel/DesktopSlidingPanel.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import OpenArrowIcon from "../../assets/icons/OpenArrowIcon.svg";
+import CloseArrowIcon from "../../assets/icons/CloseArrowIcon.svg";
+import styled from "styled-components";
+import { colors } from "../../styles/theme";
+
+interface DesktopSlidingPanelProps {
+  content?: React.ReactNode;
+}
+
+const DesktopSlidingPanel: React.FC<DesktopSlidingPanelProps> = ({
+  content,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Wrapper>
+      <PanelContainer isOpen={isOpen}>
+        <Content>{content}</Content>
+      </PanelContainer>
+      <PanelToggle isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}>
+        <img src={isOpen ? CloseArrowIcon : OpenArrowIcon} />
+      </PanelToggle>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+`;
+
+const PanelContainer = styled.div<{ isOpen: boolean }>`
+  width: 350px;
+  height: 100vh;
+  background-color: ${colors.secondary};
+  position: absolute;
+  top: 0;
+  left: ${({ isOpen }) => (isOpen ? "0" : "-350px")};
+  transition: left 0.3s ease;
+  z-index: 5;
+`;
+
+const PanelToggle = styled.button<{ isOpen: boolean }>`
+  position: absolute;
+  top: 50%;
+  left: ${({ isOpen }) => (isOpen ? "350px" : "0")};
+  transform: translateY(-50%);
+  width: 35px;
+  height: 85px;
+  background-color: white;
+  border: none;
+  border-radius: 0 20px 20px 0;
+  box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: left 0.3s ease;
+`;
+
+const Content = styled.div`
+  padding: 20px;
+  color: white;
+`;
+
+export default DesktopSlidingPanel;
--- src/pages/SpaceMap.tsx
+++ src/pages/SpaceMap.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import NaverMap from "../components/NaverMap";
+import SlidingPanel from "../components/SlidingPanel";
 
 const SpaceMap: React.FC = () => {
   return (
     <SpaceMapContainer>
+      <SlidingPanel />
       <NaverMap />
     </SpaceMapContainer>
   );
@@ -12,6 +14,8 @@ const SpaceMap: React.FC = () => {
 const SpaceMapContainer = styled.div`
   width: 100%;
   height: 100vh;
+  position: relative;
+  overflow-y: hidden;
 `;
 
 export default SpaceMap;
